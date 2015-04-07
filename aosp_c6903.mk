719182c715e830bfbe7d547d6fdaff8e664f283b
David Viteri <davidteri91@gmail.com>
2015-04-08 00:34:07 +0200

    honami: add init.usb.rc to device specific
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- aosp_c6903.mk
+++ aosp_c6903.mk
@@ -29,7 +29,8 @@ PRODUCT_COPY_FILES += \
     device/sony/honami/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
     device/sony/honami/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
     device/sony/honami/rootdir/system/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
-    device/sony/honami/rootdir/system/etc/sensors_calib.conf:system/etc/sensors_calib.conf
+    device/sony/honami/rootdir/system/etc/sensors_calib.conf:system/etc/sensors_calib.conf \
+    device/sony/honami/rootdir/init.rhine.usb.rc:root/init.rhine.usb.rc
 
 PRODUCT_NAME := aosp_c6903
 PRODUCT_DEVICE := honami
