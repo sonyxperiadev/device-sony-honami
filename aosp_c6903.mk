46e4191bbb899d548b300112df96b94b4d9c4dcf
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-01 19:35:38 +0100

    add common git
    include common git into Android Build
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_c6903.mk
+++ aosp_c6903.mk
@@ -16,6 +16,7 @@ TARGET_KERNEL_CONFIG := aosp_rhine_honami_defconfig
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
+$(call inherit-product, device/sony/common/common.mk)
 $(call inherit-product, device/sony/rhine/device.mk)
 $(call inherit-product, vendor/sony/honami/honami-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
